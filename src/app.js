db61d79db7b533a847cedabb7d1bf1878fd2895e
tomas feloy <tfeloy@gmail.com>
2020-02-05 14:51:04 -0300

    module2

--- src/app.js
+++ src/app.js
@@ -8,8 +8,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'utf8' });
+const users = JSON.parse(userData);
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
+});
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
