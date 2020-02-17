7bbf6725e079c0d35485589b209dc28412cb81f6
Feloy, Tomas Ignacio <tomas.ignacio.feloy@accenture.com>
2020-02-17 20:21:10 -0300

    module5

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const { accounts, users, writeJSON } = require('./data'); 
 const express = require('express');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 
@@ -10,48 +12,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/savings', (req, res) => {
-  res.render('account', { account: accounts.savings });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', { account: accounts.credit });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', { account: accounts.checking });
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
-});
-
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-  const {from, to, amount } = req.body;
-  accounts[from].balance -= parseInt(amount);
-  accounts[to].balance += parseInt(amount);
-
-  writeJSON();
-
-  res.render('transfer', { message: "Transfer Completed" });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', { account: accounts.credit });
-});
-
-app.post('/payment', (req, res) => {
-  const { amount } = req.body;
-  accounts.credit.balance -= parseInt(amount);
-  accounts.credit.available += parseInt(amount);
-
-  writeJSON();
-
-  res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
 app.get('/', (req, res) => {
